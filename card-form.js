fa0ce58fb78408ad056f2e811f6c935048f87d44
evgen3 <g1619299@gmail.com>
2023-05-25 16:10:06 +0300

    added changes

--- card-form.js
+++ card-form.js
@@ -1,6 +1,20 @@
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
-    const form = document.querySelector('form');
-    console.dir(form.elements.date.value); 
+    let a1 = document.querySelector('#i-1').value;
+    console.log(a1);
+    let a2 = document.querySelector('#i-2').value;
+    console.log(a2);
+    let a3 = document.querySelector('#i-3').value;
+    console.log(a3);
+    let a4 = document.querySelector('#i-4').value;
+    console.log(a4);
+    let a5 = document.querySelector('#i-5').value;
+    console.log(a5);
 });
 
+// document.querySelector('button').onclick = myClick();
+
+// function myClick() {
+//     // let a = document.querySelector('.input').value;
+//     alert('a');
+// }
