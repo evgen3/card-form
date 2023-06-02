fb031ad765b43dacabdd65a59b7c8f90101b762e
evgen3 <g1619299@gmail.com>
2023-06-02 19:53:30 +0300

    use labels, rename ids, inputs are required, use FormData

--- card-form.js
+++ card-form.js
@@ -1,20 +1,10 @@
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
-    let a1 = document.querySelector('#i-1').value;
-    console.log(a1);
-    let a2 = document.querySelector('#i-2').value;
-    console.log(a2);
-    let a3 = document.querySelector('#i-3').value;
-    console.log(a3);
-    let a4 = document.querySelector('#i-4').value;
-    console.log(a4);
-    let a5 = document.querySelector('#i-5').value;
-    console.log(a5);
+
+    let formData = new FormData(event.target);
+    const formDataObject = {};
+    formData.forEach((value, key) => (formDataObject[key] = value));
+    console.log(formDataObject);
 });
 
-// document.querySelector('button').onclick = myClick();
 
-// function myClick() {
-//     // let a = document.querySelector('.input').value;
-//     alert('a');
-// }
