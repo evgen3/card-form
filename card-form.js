44a9ee800ab14f9f1b0e17633e8c3ba38ffb2cc7
evgen3 <g1619299@gmail.com>
2023-06-09 21:45:30 +0300

    created function validate(inputId, spanId, func)

--- card-form.js
+++ card-form.js
@@ -5,69 +5,38 @@ document.querySelector('form').addEventListener('submit', (event) => {
     const formDataObject = {};
     formData.forEach((value, key) => (formDataObject[key] = value));
 
-    const cardNumber = document.querySelector('#card-number');
-    const errorCardNumber = document.querySelector('#error-card-number');
+    const validCardNumber = validate('#card-number', '#error-card-number', validateCardNumber)
+    const validCardHolder = validate('#card-holder', '#error-card-holder', validateCardHolder);
+    const validMonth = validate('#month', '#error-month', validateMonth);
+    const validYear = validate('#year', '#error-year', validateYear);
+    const validCvv = validate('#cvv', '#error-cvv', validateCvv);
 
-    const cardNumberError = validateCardNumber(cardNumber.value);
-    errorCardNumber.innerText = cardNumberError;
-
-    const borderColorCardNumber = inputBorderColorCardNumber(cardNumber.value);
-    cardNumber.style.borderColor = borderColorCardNumber;
-
-
-    const cardHolder = document.querySelector('#card-holder');
-    const errorCardHolder = document.querySelector('#error-card-holder');
-
-    const cardHolderError = validateCardHolder(cardHolder.value);
-    errorCardHolder.innerText = cardHolderError;
-
-    const borderColorCardHolder = inputBorderColorCardHolder(cardHolder.value);
-    cardHolder.style.borderColor = borderColorCardHolder;
-
-   
-
-    const month = document.querySelector('#month');
-    const errorMonth = document.querySelector('#error-month');
-
-    const monthError = validateMonth(month.value);
-    errorMonth.innerText = monthError;
-
-    const borderColorMonth = inputBorderColorMonth(month.value);
-    month.style.borderColor = borderColorMonth;
-
-    
-
-    const year = document.querySelector('#year');
-    const errorYear = document.querySelector('#error-year');
-
-    const yearError = validateYear(year.value);
-    errorYear.innerText = yearError;
-
-    const borderColorYear = inputBorderColorYear(year.value);
-    year.style.borderColor = borderColorYear;
-
-    
-
-    const cvv = document.querySelector('#cvv');
-    const errorCvv = document.querySelector('#error-cvv');
-
-    const cvvError = validateCvv(cvv.value);
-    errorCvv.innerText = cvvError;
-
-    const borderColorCvv = inputBorderColorCvv(cvv.value);
-    cvv.style.borderColor = borderColorCvv;
-
-   
-
-    if (cardNumberError === '' &&
-        cardHolderError === '' &&
-        monthError === '' &&
-        yearError === '' &&
-        cvvError === '') {
+    if (validCardNumber &&
+        validCardHolder &&
+        validMonth &&
+        validYear &&
+        validCvv) {
         console.log(formDataObject);
     }
 
 });
+
+function validate(inputId, spanId, func) {
+
+    const inputItem = document.querySelector(inputId);
+    const spanItem = document.querySelector(spanId);
+
+    const errorText = func(inputItem.value);
+    spanItem.innerText = errorText;
+
+    if (errorText !== '') {
+        inputItem.style.borderColor = 'red';
+        return false;
+    } else {
+        inputItem.style.borderColor = 'green';
+        return true;
+    }
+}
 
 function removeErrorOnClick(inputId, spanId) {
     document.querySelector(inputId).addEventListener('click', (event) => {
@@ -89,27 +58,11 @@ function validateCardNumber(value) {
     }
 }
 
-function inputBorderColorCardNumber(value) {
-    if (value.length !== 16) {
-        return 'red';
-    } else {
-        return 'green';
-    }
-}
-
 function validateCardHolder(value) {
     if (value.length < 1) {
         return 'Ошибка, поле не заполнено!';
     } else {
         return '';
-    }
-}
-
-function inputBorderColorCardHolder(value) {
-    if (value.length < 1) {
-        return 'red';
-    } else {
-        return 'green';
     }
 }
 
@@ -121,14 +74,6 @@ function validateMonth(value) {
     }
 }
 
-function inputBorderColorMonth(value) {
-    if (value < 1 || value > 12) {
-        return 'red';
-    } else {
-        return 'green';
-    }
-}
-
 function validateYear(value) {
     if (value < 2023) {
         return 'Ошибка,  год должен быть от 2023!';
@@ -137,27 +82,11 @@ function validateYear(value) {
     }
 }
 
-function inputBorderColorYear(value) {
-    if (value < 2023) {
-        return 'red';
-    } else {
-        return 'green';
-    }
-}
-
 function validateCvv(value) {
     if (value.length !== 3) {
         return 'Ошибка, в CVV должно быть 3 цифры!';
     } else {
         return '';
-    }
-}
-
-function inputBorderColorCvv(value) {
-    if (value.length !== 3) {
-        return 'red';
-    } else {
-        return 'green';
     }
 }
 
